c12ba92448fac7126a5f1ec48e2ab6d61b37b943
Amirabbas Fazelinia <utilyre@proton.me>
2023-08-30 12:46:44 +0330

    test: migrate tests to use testify package

--- time_test.go
+++ time_test.go
@@ -1,58 +1,63 @@
-package sqltime_test
+package sqltime
 
 import (
-	"errors"
 	"testing"
 
-	"github.com/utilyre/sqltime"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestParse(t *testing.T) {
-	time := &sqltime.Time{}
-	if err := time.Parse("08:56:00"); err != nil {
-		t.Error(err)
+	time := Time{}
+
+	if err := time.Parse("08:56:04"); assert.Nil(t, err) {
+		assert.Equal(t, Time{hour: 8, minute: 56, second: 4}, time)
 	}
-	if time.Hour != 8 {
-		t.Errorf("time.Hour = %d; want 8", time.Hour)
+	if err := time.Parse("9:10"); assert.Nil(t, err) {
+		assert.Equal(t, Time{hour: 9, minute: 10, second: 0}, time)
 	}
-	if time.Minute != 56 {
-		t.Errorf("time.Minute = %d; want 56", time.Minute)
-	}
-	if time.Second != 0 {
-		t.Errorf("time.Second = %d; want 0", time.Second)
+	if err := time.Parse("06"); assert.Nil(t, err) {
+		assert.Equal(t, Time{hour: 6, minute: 0, second: 0}, time)
 	}
 
-	if err := time.Parse("18:05"); err != nil {
-		t.Error(err)
-	}
-	if time.Hour != 18 {
-		t.Errorf("time.Hour = %d; want 18", time.Hour)
-	}
-	if time.Minute != 5 {
-		t.Errorf("time.Minute = %d; want 5", time.Minute)
-	}
-	if time.Second != 0 {
-		t.Errorf("time.Second = %d; want 0", time.Second)
+	if err := time.Parse("7:30:13:4"); assert.NotNil(t, err) {
+		assert.ErrorIs(t, err, ErrTooManyParts)
 	}
 
-	if err := time.Parse("20"); err != nil {
-		t.Error(err)
+	if err := time.Parse("hi:5:47"); assert.NotNil(t, err) {
+		target := new(AtoiError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, target.Part, PartHour)
+		}
 	}
-	if time.Hour != 20 {
-		t.Errorf("time.Hour = %d; want 20", time.Hour)
+	if err := time.Parse("5:hi:47"); assert.NotNil(t, err) {
+		target := new(AtoiError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, target.Part, PartMinute)
+		}
 	}
-	if time.Minute != 0 {
-		t.Errorf("time.Minute = %d; want 0", time.Minute)
-	}
-	if time.Second != 0 {
-		t.Errorf("time.Second = %d; want 0", time.Second)
+	if err := time.Parse("5:47:hi"); assert.NotNil(t, err) {
+		target := new(AtoiError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, target.Part, PartSecond)
+		}
 	}
 
-	err := time.Parse("24:18:59")
-	if err == nil {
-		t.Error("time.Parse did not error")
+	if err := time.Parse("24:57:00"); assert.NotNil(t, err) {
+		target := new(RangeError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, RangeError{Part: PartHour, Min: 0, Max: 23}, *target)
+		}
 	}
-	if !errors.Is(err, sqltime.ErrRange) {
-		t.Error("time.Parse did not error sqltime.ErrRange")
+	if err := time.Parse("13:-2:06"); assert.NotNil(t, err) {
+		target := new(RangeError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, RangeError{Part: PartMinute, Min: 0, Max: 59}, *target)
+		}
+	}
+	if err := time.Parse("20:33:62"); assert.NotNil(t, err) {
+		target := new(RangeError)
+		if assert.ErrorAs(t, err, &target) {
+			assert.Equal(t, RangeError{Part: PartSecond, Min: 0, Max: 59}, *target)
+		}
 	}
 }
