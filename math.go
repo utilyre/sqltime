030ae972d1f21a8243283d2a24f651f870d14480
Amirabbas Fazelinia <utilyre@proton.me>
2023-09-01 10:55:52 +0330

    perf(math): make `div` function branchless

--- math.go
+++ math.go
@@ -2,12 +2,7 @@ package sqltime
 
 // See https://github.com/golang/go/issues/448
 func div(a, b int) int {
-	c := a / b
-	if a%b < 0 {
-		c -= 1
-	}
-
-	return c
+	return (a - mod(a, b)) / b
 }
 
 // See https://github.com/golang/go/issues/448
